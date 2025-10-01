ec29feeb474d964ddf63e8a7c09bfd9efa09e305
nisaking03 <nisa.king03@gmail.com>
2025-10-01 11:43:05 -0400

    Matt walk through

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -6,31 +6,32 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        //Variables
-        String firstName = arrayedName[0];
-        String middleName;
-        String lastName;
-        String fullName = sc.nextLine();;
-        String[] arrayedName = fullName.split("");
-
-        //Prompt user
-
+        // Setting up name anatomy
         System.out.print("Type in your full name please: ");
+        String fullName = sc.nextLine();
 
-        //Put it together
-        System.out.println("Your full name is: " + fullname);
+        System.out.println(fullName);
 
-        firstName =
-        System.out.println("Your first name is: " + firstName);
+        String[] nameParts = fullName.split(" ");
+        String firstName = nameParts[0];
 
-        System.out.println("Your middle name is: " + middleName);
+        String middleName = "";
+        String lastName;
 
-        System.out.println("Your last name is: " + lastName);
+        // If statements for middle name
+        if(nameParts.length == 2){
+            middleName = "";
+            lastName = nameParts[1];
+        }
+        else{
+            middleName = nameParts[1];
+            lastName = nameParts[2];
+        }
 
-
-
-
-        //Display
+        //Displaying
+        System.out.println("First Name: " + firstName);
+        System.out.println("Middle Name: " + middleName);
+        System.out.println("Last Name: " + lastName);
 
 
 
