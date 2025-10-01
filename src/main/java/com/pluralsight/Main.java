9963a3c8df7912df14a793978b5ea8c613da1cdf
nisaking03 <nisa.king03@gmail.com>
2025-10-01 10:46:19 -0400

    made changes

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,38 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //Variables
+        String firstName = arrayedName[0];
+        String middleName;
+        String lastName;
+        String fullName = sc.nextLine();;
+        String[] arrayedName = fullName.split("");
+
+        //Prompt user
+
+        System.out.print("Type in your full name please: ");
+
+        //Put it together
+        System.out.println("Your full name is: " + fullname);
+
+        firstName =
+        System.out.println("Your first name is: " + firstName);
+
+        System.out.println("Your middle name is: " + middleName);
+
+        System.out.println("Your last name is: " + lastName);
+
+
+
+
+        //Display
+
+
+
+    }
+}
